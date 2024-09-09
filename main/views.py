d04511baa7a27f5dae1e0d6a9236e91935834026
salsaaruum <tatasalsa04@gmail.com>
2024-09-09 13:19:23 +0700

    Menambahkan beberapa menu baru

--- main/views.py
+++ main/views.py
@@ -5,7 +5,23 @@ def show_main(request):
         'name' : 'Blueberry Cheesecake',
         'price': 30000,
         'description': 'Cheesecake lembut creamy dengan topping saus blueberry',
-        'category' : 'Dessert'
+        'category' : 'Dessert',
+        'space' : '==============================================================='
     }
 
-    return render(request, "main.html", {'blueberry_cheesecake': blueberry_cheesecake})
+    cromboloni = {
+        'name' : 'Cromboloni',
+        'price': 40000,
+        'description': 'Cromboloni yang renyah dengan pilihan isian biscoff, redvelvet, dan pistachio',
+        'category' : 'Dessert',
+        'space' : '==============================================================='
+    }
+
+    croissant = {
+        'name' : 'Croissant',
+        'price': 25000,
+        'description': 'Makanan khas Prancis dengan rasa butter klasik',
+        'category' : 'Dessert',
+        'space' : '==============================================================='
+    }
+    return render(request, "main.html", {'blueberry_cheesecake': blueberry_cheesecake, 'cromboloni': cromboloni, 'croissant': croissant})
