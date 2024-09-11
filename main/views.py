95d50b6f1813842c41fa6c7088c183f9eedd6f09
salsaaruum <tatasalsa04@gmail.com>
2024-09-11 08:59:04 +0700

    Tugas individu 2 PBP selesai

--- main/views.py
+++ main/views.py
@@ -7,7 +7,7 @@ def show_main(request):
         'description': 'Cheesecake lembut creamy dengan topping saus blueberry',
         'category' : 'Dessert',
         'image' : 'https://assets.makobakery.com/cdn/web/product_photo/1621578219_blueberry-cheesecake-slice.jpg',
-        'space' : '=========================================================================='
+        'space' : '========================================================================================================================='
     }
 
     tiramisu = {
@@ -16,7 +16,7 @@ def show_main(request):
         'description': 'Tiramisu dengan tekstur yang lembut dan rasa yang menyegarkan, menjadikannya salah satu hidangan penutup favorit',
         'category' : 'Dessert',
         'image' : 'https://nibble-images.b-cdn.net/nibble/original_images/tiramisu_paling_enak_di_jakarta_1_9e8705662b_qhG4omixx.jpg',
-        'space' : '=========================================================================='
+        'space' : '========================================================================================================================='
     }
 
     croissant = {
@@ -25,6 +25,6 @@ def show_main(request):
         'description': 'Makanan khas Prancis dengan rasa butter klasik',
         'category' : 'Dessert',
         'image' : 'https://www.aliceboulangerie.com.sg/wp-content/uploads/2023/01/Classic-Croissant--scaled.jpg',
-        'space' : '=========================================================================='
+        'space' : '========================================================================================================================='
     }
     return render(request, "main.html", {'blueberry_cheesecake': blueberry_cheesecake, 'tiramisu': tiramisu, 'croissant': croissant})
